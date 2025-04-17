125a9adb2bd24d44094f991b316b00e5503d5e12
Jeff Garris <jeffsgarris@gmail.com>
2025-04-17 01:29:00 -0400

    Handle list item overflow

--- src/components/ListItems.tsx
+++ src/components/ListItems.tsx
@@ -4,7 +4,7 @@ import ListItem from "./ListItem";
 export default function ListItems() {
   const { lists, selectedListID } = useListsContext();
   return (
-    <ul>
+    <ul className="overflow-y-auto h-[488px]">
       {lists.find((list) => list.id === selectedListID)?.items.length === 0 ? (
         <p className="flex items-center justify-center mt-4 font-semibold italic text-gray-400">
           No items in this list. Add some items to get started!
