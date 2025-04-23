981f28e93c2493a94dfd41532f69c61252d1034d
Jeff Garris <jeffsgarris@gmail.com>
2025-04-23 12:21:32 -0400

    Missed this one

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import Header from "./components/Header";
 import List from "./components/List";
 import "./App.css";
-import ListsMenu from "./components/ListsMenu";
-import AddListForm from "./components/AddListForm";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
@@ -10,40 +9,7 @@ function App() {
       <Header />
 
       <div className="flex h-full relative">
-        {/* Hamburger Icon */}
-        {isMobile && (
-          <button
-            className="absolute top-2 left-2 z-20 flex flex-col justify-between w-8 h-6 p-1 focus:outline-none"
-            onClick={() => setMenuOpen(!menuOpen)}
-          >
-            <span className="w-full h-[3px] bg-gray-800 rounded"></span>
-            <span className="w-full h-[3px] bg-gray-800 rounded"></span>
-            <span className="w-full h-[3px] bg-gray-800 rounded"></span>
-          </button>
-        )}
-
-        {/* Sidebar */}
-        <div
-          className={`
-            flex flex-col 
-            border-r border-r-gray-200 bg-white 
-            z-10 transition-all duration-300 ease-in-out
-            ${
-              isMobile
-                ? menuOpen
-                  ? "absolute top-0 left-0 w-[250px] h-full"
-                  : "w-[50px] min-w-[50px] flex-shrink-0"
-                : "w-[250px] relative"
-            }
-          `}
-        >
-          {(menuOpen || !isMobile) && (
-            <>
-              <AddListForm />
-              <ListsMenu />
-            </>
-          )}
-        </div>
+        <SideBar />
 
         {/* Main content */}
         <div className="flex-1">
