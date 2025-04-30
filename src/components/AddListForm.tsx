c595a4d1d181039fa69d94bd35ee1585e6c70775
Jeff Garris <jeffsgarris@gmail.com>
2025-04-30 11:30:50 -0400

    Input, button, and list item border updates

--- src/components/AddListForm.tsx
+++ src/components/AddListForm.tsx
@@ -21,7 +21,8 @@ export default function AddListForm() {
           type="text"
           value={listName}
           onChange={(e) => setListName(e.target.value)}
-          className="px-4 py-2 border border-gray-300 focus:border-[#F56A3E] bg-gray-50 rounded outline-0 transition-colors duration-200 ease-in-out"
+          placeholder="Add a new list"
+          className="px-4 py-2 border border-transparent focus:border-[#F56A3E] empty:text-gray-500 bg-slate-100 rounded outline-0 transition-colors duration-200 ease-in-out"
         />
         <Button>Add List</Button>
       </label>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({ children, buttonType, onClick }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className={`py-2 px-6 border border-[#F56A3E] rounded ${
+      className={`min-w-[120px] py-2 px-6 border border-[#F56A3E] rounded ${
         buttonType === "secondary"
           ? "bg-white text-[#F56A3E] hover:bg-[#F56A3E] hover:text-white"
           : "bg-[#F56A3E] text-white hover:bg-white hover:text-[#F56A3E]"
